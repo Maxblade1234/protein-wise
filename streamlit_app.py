e7fb5b4ecb9e867ab028d2e3a56aec41a02a1e0c
Maxblade1234 <117409768+Maxblade1234@users.noreply.github.com>
2023-08-06 23:42:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+from stmol import showmol
+import py3Dmol
+import requests
+import biotite.structure.io as bsio
 
-st.title('🎈 App Name')
+#st.set_page_config(layout = 'wide')
+st.sidebar.title('🧪 ProteinWise')
+st.sidebar.write('[*ProteinWise*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
-st.write('Hello world!')
+
+if not predict:
+    st.warning('👈 Enter protein sequence data!')
